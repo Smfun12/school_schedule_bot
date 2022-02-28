341de3b71d3b87c09e45f8a52899db2fa84edb34
Bohdan Romanchuk <47035610+BRomanchuk@users.noreply.github.com>
2022-02-28 16:20:31 +0200

    finish sheet processing

--- SheetsProcessor.py
+++ SheetsProcessor.py
@@ -15,37 +15,53 @@ table = client.open(MAIN_TABLE)
 
 class Frame:
     def __init__(self):
-        self.sheet = table.worksheet('sheet_one')
+        self.sheet = table.worksheet('data')
         data = np.array(self.sheet.get_all_values())
         records = data[1:]
         columns = data[0]
         self.df = pd.DataFrame(records, columns=columns)
+        self.users_num = self.df.shape[0] - (self.df['telegram_id'] == '').sum()
 
 
 frame = Frame()
 
 
 def get_login_password(telegram_id):
+    """
+    Takes telegram id of the user and returns their login and password
+    Registers new users
+    :param telegram_id: telegram id of the user
+    :return: login, password
+    """
+    user_idx = frame.df[frame.df['telegram_id'] == str(telegram_id)].index
+    if user_idx.shape[0] == 0:
+        __register_user(telegram_id)
+        user_idx = [frame.users_num]
+    user = frame.df.iloc[user_idx].to_numpy()[0]
+    return user[0], user[1]
+
+
+def __register_user(telegram_id):
+    __update_cell(frame.users_num, 2, telegram_id)
+
+
+def __is_registered(telegram_id):
     user = frame.df[frame.df['telegram_id'] == str(telegram_id)]
     if user.shape[0] == 0:
-        register_user(telegram_id)
-
-
-def register_user(telegram_id):
-    pass
-
-
-def is_registered(telegram_id):
-    return False
+        return False
+    return True
 
 
 def __get_user_row(telegram_id):
-    pass
+    return frame.df[frame.df['telegram_id'] == str(telegram_id)].index[0]
 
 
 def __update_cell(i, j, value):
-    pass
+    y, x = __sheet_coords(i, j)
+    frame.sheet.update_cell(y, x, str(value))
+    frame.df.iloc[i, j] = str(value)
 
 
 def __sheet_coords(i, j):
     return i+2, j+1
+
--- constant.py
+++ constant.py
@@ -1,13 +1,13 @@
 # Change token
-TOKEN ='5253652716:AAEvUly9LXT9-UzP9MxKsCwnNcLW3HiaRH0'
+TOKEN = '5253652716:AAEvUly9LXT9-UzP9MxKsCwnNcLW3HiaRH0'
 
 SCOPE = [
     "https://spreadsheets.google.com/feeds",
     'https://www.googleapis.com/auth/spreadsheets',
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-
 ]
+
 JSON_ROUTE = "CheQuest.json"
 
-MAIN_TABLE = 'MY_QUEST'
+MAIN_TABLE = 'test_sheet_'
